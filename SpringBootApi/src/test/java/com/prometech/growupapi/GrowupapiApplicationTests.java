c58b21de85f0dd9053c741380116c8bb8e34b014
Lucas Bueno <lucasbueno2004@hotmail.com>
2025-07-08 11:51:42 -0300

    adding api repo

---
+++ SpringBootApi/src/test/java/com/prometech/growupapi/GrowupapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prometech.growupapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowupapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
